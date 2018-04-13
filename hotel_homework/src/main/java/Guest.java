6dd166a6fe7745b60acb3f610118514745edd863
Claire <cconnachan@gmail.com>
2018-04-13 16:53:43 +0100

    wrote guest class, passed getName test

--- hotel_homework/src/main/java/Guest.java
+++ hotel_homework/src/main/java/Guest.java
@@ -1,2 +1,16 @@
 public class Guest {
+
+    private String name;
+    private double wallet;
+
+    public Guest(String name, double wallet){
+        this.name = name;
+        this.wallet = wallet;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+
 }
--- hotel_homework/src/test/java/GuestTest.java
+++ hotel_homework/src/test/java/GuestTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    private Guest skintGuest;
+    private Guest richGuest;
+
+    @Before
+    public void before(){
+        skintGuest = new Guest("Simon", 0.0);
+        richGuest = new Guest("Claire", 100.0);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Simon",skintGuest.getName());
+        assertEquals("Claire", richGuest.getName());
+    }
+
+
 }
