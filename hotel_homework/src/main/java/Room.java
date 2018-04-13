0a07957fba646f67e3e3261ebb946b958c9e1c0a
Claire <cconnachan@gmail.com>
2018-04-13 15:10:00 +0100

    started project classes and tests skeleton

---
+++ hotel_homework/src/main/java/Room.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public abstract class Room {
+
+    private ArrayList<Guest> occupants;
+    private String name;
+
+    public Room(String name){
+        this.name = name;
+        this.occupants = new ArrayList<>();
+    }
+
+
+
+
+
+
+
+
+
+
+}
