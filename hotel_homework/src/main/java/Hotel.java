6534cf69a245ad147a256104271f89bfe03bba8a
Claire <cconnachan@gmail.com>
2018-04-14 13:27:15 +0100

    added hotel class, passed getName test;

--- hotel_homework/src/main/java/Hotel.java
+++ hotel_homework/src/main/java/Hotel.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class Hotel {
+
+    private String name;
+    private double till;
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+    private ArrayList<Room> otherRooms;
+
+    public Hotel(String name, double till){
+        this.name = name;
+        this.till = till;
+        this.bedrooms = new ArrayList<>();
+        this.conferenceRooms = new ArrayList<>();
+        this.otherRooms = new ArrayList<>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
 }
--- hotel_homework/src/test/java/HotelTest.java
+++ hotel_homework/src/test/java/HotelTest.java
@@ -1,2 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelTest {
+
+    private Hotel hotel;
+    private Guest skintGuest;
+    private Guest richGuest;
+    private Bedroom singleBedroom;
+    private Bedroom doubleBedroom;
+
+
+    @Before
+    public void before(){
+        hotel = new Hotel("Happiness Hotel", 0.0);
+        skintGuest = new Guest("Simon", 0.0);
+        richGuest = new Guest("Claire", 100.0);
+        singleBedroom = new Bedroom("1", RoomType.SINGLE, 25.0);
+        doubleBedroom = new Bedroom("2", RoomType.DOUBLE, 50.0);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Happiness Hotel",hotel.getName());
+    }
+
+    
+
+
+
 }
